12bc2621fcf789def5e4fe892bd367478474bd17
Alex <alexwood@languagekey.com>
2025-02-27 08:25:55 +0800

    installed framer motion and added video section draft layout

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import HeroSection from './components/HeroSection';
+import Video from './components/Video';
 // No need to import Navbar as it's in the shared layout
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <HeroSection />
+        <Video />
         
         {/* Additional home page sections can go here */}
       </div>
